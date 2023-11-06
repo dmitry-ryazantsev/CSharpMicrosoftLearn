7114898f61f7831a2d4b1a5f8fefa3129d5a21d7
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-06 11:46:50 +0200

    Complete a variable scope challenge

--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
-﻿using System.Security;
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
-string permission = "Admin|Manager";
-int level = 55;
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
 
-if (permission.Contains("Admin") && level > 55)
-    Console.WriteLine("Welcome, Super Admin user.");
+if (found)
+    Console.WriteLine("Set contains 42");
 
-else if (permission.Contains("Admin") && level <= 55)
-    Console.WriteLine("Welcome, Admin user.");
-
-else if (permission.Contains("Manager") && level >= 20)
-    Console.WriteLine("Contact an Admin for access.");
-
-else if (permission.Contains("Manager") && level < 20)
-    Console.WriteLine("You do not have sufficient privileges.");
-
-else if (!permission.Contains("Manager") || !permission.Contains("Admin"))
-    Console.WriteLine("You do not have sufficient privileges.");
+Console.WriteLine($"Total: {total}");
