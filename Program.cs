7afcdcdda145c2907ce5f3c538d41850b4dd2ab0
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-07 09:41:21 +0200

    Create a for loop

--- Program.cs
+++ Program.cs
@@ -1,56 +1,7 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type;
-string color;
-string size;
-
-switch (product[0])
+﻿string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
 {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+    if (names[i] == "David")
+        names[i] = "Sammy";
+    Console.WriteLine(names[i]);
 }
-
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-}
-
-switch (product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
