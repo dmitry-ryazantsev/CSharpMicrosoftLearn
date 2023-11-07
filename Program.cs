7c4b0c8f72d41dc2b28e3c4d79bb1d417ab2d1eb
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-07 11:58:26 +0200

    Implement a do-while loop

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿Random random = new Random();
+int current = 0;
+
+do
 {
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine($"{i} - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine($"{i} - Buzz");
-    else
-        Console.WriteLine(i);
-}
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
