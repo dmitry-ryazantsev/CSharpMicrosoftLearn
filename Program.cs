153d31a6dc9cd6e1fe063b2d1f9a85f22508d63b
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-07 13:14:37 +0200

    Complete game battle with a while loop challenge

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿Random random = new Random();
-int current = 0;
+﻿Random dice = new Random();
+int heroHealthPoints = 10;
+int monsterHealthPoitns = 10;
 
-do
+while (heroHealthPoints > 0 && monsterHealthPoitns > 0)
 {
-    current = random.Next(1, 11);
-    Console.WriteLine(current);
-} while (current != 7);
+    int attack = dice.Next(1, 11);
+    monsterHealthPoitns -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monsterHealthPoitns} health");
+
+    if (monsterHealthPoitns <= 0) break;
+
+    attack = dice.Next(1, 11);
+    heroHealthPoints -= attack;
+    Console.WriteLine($"Hero was damaged and lost {attack} health and now has {heroHealthPoints} health");
+}
+
+Console.WriteLine(heroHealthPoints > monsterHealthPoitns ? "Hero wins!" : "Monster wins!");
