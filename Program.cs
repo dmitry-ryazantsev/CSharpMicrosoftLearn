3bb3719805e6c93a24c92d5dd0e1e5fcddb9da22
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-22 18:46:51 +0200

    Challenge: outputh math operations as specific number types

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
-﻿// Running the program in en-US culture setting to handle a period as the decimal separator
-using System.Globalization;
-CultureInfo.CurrentCulture = new CultureInfo("en-US");
+﻿int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-string message = "";
-float total = 0.0f;
+int result1 = value1 / Convert.ToInt32(value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-foreach (string value in values)
-{
-    if (float.TryParse(value, out float number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += value;
-    }
-}
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
