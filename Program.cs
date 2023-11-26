be6151c62269f2f7af9519f63af587ea177bf341
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-26 19:47:37 +0200

    Format a shares purchase receipt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N} units");
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P}");
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
