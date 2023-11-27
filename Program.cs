de4c92d4450b57ca1acfcc882356bd7268768d91
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-11-27 18:49:29 +0200

    Discover padding and alignment

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿using System.Globalization;
-CultureInfo.CurrentCulture = new CultureInfo("en-US");
+﻿string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
 
-int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Shares: {productShares:N3} Product");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
