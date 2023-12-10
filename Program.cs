f5ee649ce4d4c8a5d8cec7caf24c36e3f8ce8dc8
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-12-10 19:41:17 +0200

    Return numbers from methods

--- Program.cs
+++ Program.cs
@@ -1,32 +1,17 @@
-﻿string[,] corporate =
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd)
 {
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
-};
-
-string[,] external =
-{
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
-
-string externalDomain = "hayworth.com";
-
-DisplayAllEmails(externalDomain);
-
-void DisplayAllEmails(string externalDomain, string internalDomain = "contoso.com")
-{
-    PrintEmails(corporate, internalDomain);
-    PrintEmails(external, externalDomain);
+    int rate = 23500;
+    return (int)(rate * usd);
 }
 
-void PrintEmails(string[,] group, string domain)
+double VndToUsd(int vnd)
 {
-    for (int i = 0; i < group.GetLength(0); i++)
-    {
-        // first two letters of the first name and entire last name
-        string email = group[i, 0].Substring(0, 2) + group[i, 1] + "@" + domain;
-        Console.WriteLine(email.ToLower());
-    }
+    double rate = 23500;
+    return vnd / rate;
 }
